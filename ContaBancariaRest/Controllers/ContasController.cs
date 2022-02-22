c9b4a4a005cb50123e247b4f0b598e2de942c5c5
felipe.pascoal <bd.bumdigital@gmail.com>
2022-02-22 11:23:44 -0300

    logica da conta bancaria

---
+++ ContaBancariaRest/Controllers/ContasController.cs
@@ -0,0 +1,75 @@
+﻿using ContaBancariaRest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContaBancariaRest.Controllers
+{
+    [ApiController]
+    public class ContasController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/contas")]
+        public IActionResult Index()
+        {
+            var contas = Conta.Contas;
+            return StatusCode(200, contas);
+        }
+
+        [HttpPost]
+        [Route("/contas")]
+        public IActionResult Create([FromBody] Conta conta)
+        {
+            Conta.Contas.Add(conta);
+            return StatusCode(201, conta);
+        }
+
+
+        [HttpPut]
+        [Route("/contas/depositar/{id}")]
+        public IActionResult Update(int id, [FromBody] Conta depositar )
+        {
+            var achei = false;
+            foreach (var c in Conta.Contas)
+            {
+                if (c.Numero == id)
+                {
+                    c.Saldo += depositar.Depositar;
+                    c.Mensagem = "Deposito realizado com sucesso!";
+                    achei = true;
+                }
+            }
+
+            if (!achei) return StatusCode(404);
+            else return StatusCode(204);
+        }
+
+        [HttpPut]
+        [Route("/contas/sacar/{id}")]
+        public IActionResult UpdateSacar(int id, [FromBody] Conta sacar)
+        {
+            var achei = false;
+            foreach (var c in Conta.Contas)
+            {
+                if (c.Numero == id && sacar.Sacar <= c.Saldo)
+                {
+                    c.Saldo -= sacar.Sacar;
+                    c.Mensagem = "Saque realizado com sucesso!";
+                    achei = true;
+                } else if (c.Numero == id && sacar.Sacar > c.Saldo)
+                {
+                    c.Mensagem = "Saldo insuficiente para essa operação";
+                }
+            }
+
+            if (!achei) return StatusCode(404);
+            else return StatusCode(204);
+        }
+    }
+}       
+            
+        
+
---
+++ ContaBancariaRest/Models/Contas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContaBancariaRest.Models
+{
+    public class Conta
+    {
+        public string Mensagem { get; set; }
+        public int Numero { get; set; }
+        public float Saldo { get; set; }
+
+        public string Titular { get; set; }
+        public float Depositar { get; set; }
+        public float Sacar { get; set; }
+
+        public static List<Conta> Contas = new List<Conta>();
+    }
+}
