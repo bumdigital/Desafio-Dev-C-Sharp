3b99a05093a1ecacb1abcd8e0d883464d05327a1
felipe.pascoal <bd.bumdigital@gmail.com>
2022-02-23 16:11:32 -0300

    gravando dados no SQL

--- ContaBancariaRest/Controllers/ContasController.cs
+++ ContaBancariaRest/Controllers/ContasController.cs
@@ -11,11 +11,21 @@ namespace ContaBancariaRest.Controllers
     [ApiController]
     public class ContasController : ControllerBase
     {
+        private DBContexto _contexto;
+        public ContasController(DBContexto contexto)
+        {
+            this._contexto = contexto;
+        }
         [HttpGet]
         [Route("/contas")]
         public IActionResult Index()
         {
-            var contas = Conta.Contas;
+            //SOLUÇÃO PARA GRAVAR DADOS EM MEMÓRIA
+            //var contas = Conta.Contas;
+            //return StatusCode(200, contas);
+
+            //SOLUÇÃO PARA GRAVAR DADOS NO SQL
+            var contas = _contexto.Contas.ToList();
             return StatusCode(200, contas);
         }
 
@@ -23,7 +33,12 @@ namespace ContaBancariaRest.Controllers
         [Route("/contas")]
         public IActionResult Create([FromBody] Conta conta)
         {
-            Conta.Contas.Add(conta);
+            //SOLUÇÃO PARA GRAVAR DADOS EM MEMÓRIA
+            //Conta.Contas.Add(conta);
+
+            //SOLUÇÃO PARA GRAVAR DADOS NO SQL
+            _contexto.Contas.Add(conta);
+            _contexto.SaveChanges();
             return StatusCode(201, conta);
         }
 
@@ -32,6 +47,7 @@ namespace ContaBancariaRest.Controllers
         [Route("/contas/depositar/{id}")]
         public IActionResult Update(int id, [FromBody] Conta depositar )
         {
+            /* SOLUÇÃO PARA GRAVAR DADOS EM MEMÓRIA
             var achei = false;
             foreach (var c in Conta.Contas)
             {
@@ -44,13 +60,27 @@ namespace ContaBancariaRest.Controllers
             }
 
             if (!achei) return StatusCode(404);
-            else return StatusCode(204);
+            else return StatusCode(200, depositar);*/
+
+            //SOLUÇÃO PARA GRAVAR DADOS NO SQL
+            var conta = _contexto.Contas.Where(c => c.Numero == id).First();
+            if (conta == null) 
+            {
+                return StatusCode(404);
+            }
+
+            conta.Saldo += (decimal)depositar.Depositar;
+            conta.Mensagem = "Deposito realizado com sucesso!";
+            _contexto.Contas.Update(conta);
+            _contexto.SaveChanges();
+            return StatusCode(200, conta);
         }
 
         [HttpPut]
         [Route("/contas/sacar/{id}")]
         public IActionResult UpdateSacar(int id, [FromBody] Conta sacar)
         {
+            /* SOLUÇÃO PARA GRAVAR DADOS EM MEMÓRIA
             var achei = false;
             foreach (var c in Conta.Contas)
             {
@@ -66,7 +96,26 @@ namespace ContaBancariaRest.Controllers
             }
 
             if (!achei) return StatusCode(404);
-            else return StatusCode(204);
+            else return StatusCode(200, sacar);*/
+
+            //SOLUÇÃO PARA GRAVAR DADOS NO SQL
+            var conta = _contexto.Contas.Where(c => c.Numero == id).First();
+            if (conta == null)
+            {
+                return StatusCode(404);
+            }
+
+            if (conta.Saldo >= (decimal)sacar.Sacar) 
+            {
+                conta.Saldo -= (decimal)sacar.Sacar;
+                conta.Mensagem = "Saque realizado com sucesso!";
+                _contexto.Contas.Update(conta);
+                _contexto.SaveChanges();
+                return StatusCode(200, conta);
+            } else
+            {
+                return StatusCode(400, "Saldo insuficiente para essa transação");
+            }
         }
     }
 }       
--- ContaBancariaRest/Models/Conta.cs
+++ ContaBancariaRest/Models/Conta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,19 @@ namespace ContaBancariaRest.Models
 {
     public class Conta
     {
-        public string Mensagem { get; set; }
+        [Key]
         public int Numero { get; set; }
-        public float Saldo { get; set; }
-
+        public decimal Saldo { get; set; }
         public string Titular { get; set; }
+
+
+
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+        [NotMapped]
         public float Depositar { get; set; }
+        [NotMapped]
         public float Sacar { get; set; }
 
         public static List<Conta> Contas = new List<Conta>();
